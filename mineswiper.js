70511bc8113fee43498c777fceb51a9424c906ce
marininik <m.nikolli@lswr.it>
2022-01-19 11:31:22 +0100

    mineswiper

---
+++ mineswiper.js
@@ -0,0 +1,125 @@
+export const TILE_STATUSES = {
+    HIDDEN: 'hidden',
+    MINE: 'mine',
+    NUMBER: 'number',
+    MARKED: 'marked'
+}
+export function createBoard(boardSize, numberOfMines) {
+    const board = [];
+    const minePosition = getMinePositions(boardSize, numberOfMines);
+    // console.log(minePosition);
+    for (let x = 0; x < boardSize; x++) {
+        const row = [];
+        for (let y = 0; y < boardSize; y++) {
+            const element = document.createElement('div');
+            element.setAttribute('class', 'bordered'),
+
+                element.dataset.status = TILE_STATUSES.HIDDEN;
+            const tile = {
+                element,
+                x,
+                y,
+                mine: minePosition.some(positionMatch.bind(null, {
+                    x,
+                    y
+                })),
+                get status() {
+                    return this.element.dataset.status
+                },
+                set status(value) {
+                    this.element.dataset.status = value;
+                }
+            };
+            row.push(tile);
+        }
+        board.push(row)
+    }
+    return board
+}
+
+export function markTile(tile) {
+    if (tile.status !== TILE_STATUSES.HIDDEN && tile.status !== TILE_STATUSES.MARKED) {
+        return;
+    }
+
+    if (tile.status === TILE_STATUSES.MARKED) {
+        tile.status = TILE_STATUSES.HIDDEN;
+    } else {
+        tile.status = TILE_STATUSES.MARKED;
+    }
+}
+
+export function revealTile(tile, board) {
+    if (tile.status !== TILE_STATUSES.HIDDEN) {
+        return;
+    }
+    if (tile.mine) {
+        tile.status = TILE_STATUSES.MINE;
+        return;
+    }
+    tile.status = TILE_STATUSES.NUMBER;
+
+    const adjentTiles = nearbyTiles(board, tile);
+    const mines = adjentTiles.filter(t => t.mine);
+    if (mines.length === 0) {
+        adjentTiles.forEach(revealTile.bind(null, board))
+    } else [
+        tile.element.textContent = mines.length
+    ]
+}
+
+export function checkWin(board) {
+    board.every(row => {
+        return row.every(tile => {
+            return tile.status === TILE_STATUSES.NUMBER || (tile.mine && (tile.status === TILE_STATUSES.HIDDEN || tile.status === TILE_STATUSES.MARKED))
+        })
+    })
+}
+export function checkLose(board) {
+    board.some(row => {
+        row.some(tile => {
+            return tile.status === TILE_STATUSES.MINE
+        })
+    })
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+    const positions = [];
+    while (positions.length < numberOfMines) {
+        const position = {
+            x: randomNumber(boardSize),
+            y: randomNumber(boardSize)
+        }
+        if (!positions.some(positionMatch.bind(null, position))) {
+            positions.push(position);
+        }
+    }
+    return positions;
+}
+
+function positionMatch(a, b) {
+    return a.x === b.x && a.y === b.y;
+}
+
+function randomNumber(size) {
+    return Math.floor(Math.random() * size);
+}
+
+
+function nearbyTiles(board, {
+    x,
+    y
+}) {
+    const tiles = [];
+    for (let xOffset = -1; xOffset < 1; xOffset++) {
+        for (let yOffset = -1; yOffset < 1; yOffset++) {
+            const tile = board[x + xOffset]?.[y + yOffset];
+            if (tile) tiles.push(tile)
+                
+            // }
+            // tiles.push(tile)
+        }
+
+    }
+    return tiles;
+}
